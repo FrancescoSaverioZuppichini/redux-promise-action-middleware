6459b1e6bc3fc108b703fdbc6280f08d7b14bbaf
VaeVictis <VaeVictis@VaeVictiss-MacBook-Pro.local>
2017-08-20 18:16:03 +0200

    minor changes

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const promiseMiddleware = store => next => action => {
-    // check if it has a promise
-    if(!action.promise || typeof action.promise != 'function') return next(action)
-
+  // check if it has a promise
+  if(!action.promise || typeof action.promise.then != 'function') return next(action)
+    
     next({type:`${action.type}_LOADING`})
     
-    action.promise()
+    action.promise
     .then(({ data }) => next({type:`${action.type}_SUCCESS`,data}))
     .catch(err => next({type:`${action.type}_FAILURE`,err}))
-  }
 
-  export default promiseMiddleware
+}
+
+export default promiseMiddleware
